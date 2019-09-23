414c07f8477974b227f6c0a9310160dff2dd8684
gskriotor <54649017+gskriotor@users.noreply.github.com>
2019-09-23 14:54:12 -0700

    Update wfp_scripts.php

--- incs/wfp_scripts.php
+++ incs/wfp_scripts.php
@@ -2,9 +2,9 @@
 
 // register jquery and style on initialization
 function wfp_add_scripts() {
-    wp_enqueue_script( 'jDex', plugins_url('incs/js/wfp_index.js', __FILE__), array( 'jquery' ), '1.0.0', true );
+    wp_enqueue_script( 'jDex', plugins_url('js/wfp_index.js', __FILE__), array( 'jquery' ), '1.0.0', true );
 
-    wp_register_style( 'iStyle', plugins_url('incs/css/wfp_style.css', __FILE__), false,  '1.0.0', 'all');
+    wp_register_style( 'iStyle', plugins_url('css/wfp_style.css', __FILE__), false,  '1.0.0', 'all');
 }
 
 // use the registered jquery and style above
